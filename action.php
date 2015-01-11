cd04fc7321f6e6564f7746958c955263602b0f27
Faizan Ayubi <indianayubi@gmail.com>
2015-01-11 14:54:41 +0530

    Update action.php

--- action.php
+++ action.php
@@ -2,32 +2,21 @@
 	require_once "spoj.php";
 	header("content-type: application/json");
 	$obj = "";
-
-	switch ($_SERVER['REQUEST_METHOD']) {
-		case 'POST':
-			$action = mysql_real_escape_string(htmlentities(trim($_POST['action'])));
-			$user = mysql_real_escape_string(htmlentities(trim($_POST['user'])));
-			break;
-		case 'GET':
-			$action = mysql_real_escape_string(htmlentities(trim($_GET['action'])));
-			$user = mysql_real_escape_string(htmlentities(trim($_GET['user'])));
-			break;
-	}
-
-	switch ($action) {
+	$user = $_GET['user'];
+	switch ($_GET['action']) {
 		case 'user_info':
 			if (isset($user)) {
 				$obj = array();
 				$handle=new SPOJ($user);
 				$obj = array(
 					'WorldRank' 		=> $handle->getWorldRank(),
-					'Points' 			=> $handle->getPoints(),
+					'Points' 		=> $handle->getPoints(),
 					'SolvedProblems'	=> $handle->getSolvedProblems(),
-					'Country'			=> $handle->getCountry(),
+					'Country'		=> $handle->getCountry(),
 					'Institution'		=> $handle->getInstitution(),
 					'TotalSolved'		=> $handle->getTotalSolved(),
 					'TotalSubmitted'	=> $handle->getTotalSubmitted(),
-					'Accepted'			=> $handle->getAC(),
+					'Accepted'		=> $handle->getAC(),
 					'WrongAnswer'		=> $handle->getWA(),
 					'CompilationError'	=> $handle->getCE(),
 					'RuntimeError'		=> $handle->getRE(),
